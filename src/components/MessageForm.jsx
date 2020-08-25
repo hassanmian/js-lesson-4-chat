093167492f52d5c5d85f2d2d4bd01a3ea9b45de4
Hassan Mian <mian@willandskill.se>
2020-08-25 16:30:03 +0200

    Minor styling changes

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -10,12 +10,11 @@ export default function MessageForm({handlePostMessage}) {
     }
 
     return (
-        <div className="form-inline alert alert-info">
+        <div className="form">
             <div class="form-group mb-2">
-                <label for="staticEmail2" class="sr-only">Message</label>
                 <input ref={messageInputField} type="text" readonly class="form-control" placeholder="Enter message..." />
             </div>
-            <button onClick={handleOnClick} class="btn btn-primary mb-2">Send Message</button>
+            <button onClick={handleOnClick} class="btn btn-primary btn-block mb-2">Send Message</button>
         </div>
     )
 }
